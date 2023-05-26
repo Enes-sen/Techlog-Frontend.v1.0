fd6f79b290803f2b03db182c1ffa9ef87fd38672
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-05-26 15:48:36 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,19 +7,19 @@ import { LoginGuard } from './loginform/login.guard';
 import { LoginformComponent } from './loginform/loginform.component';
 import { PostsComponent } from './posts/posts.component';
 import { RegisterformComponent } from './registerform/registerform.component';
-import { SinglepostComponent } from '../singlepost/singlepost.component';
+import { SinglepostComponent } from './singlepost/singlepost.component'; // Düzeltme: Yol düzeltildi.
 import { UploadimageComponent } from './uploadimage/uploadimage.component';
 
 const routes: Routes = [
-  {path:"posts",component:PostsComponent},
-  {path:"posts/:id",component:SinglepostComponent},
-  {path:"",redirectTo:"posts",pathMatch:"full"},
-  {path:"dashboard",component:DashboardComponent,canActivate:[LoginGuard]},
-  {path:"dashboard/uploadimg",component:UploadimageComponent,canActivate:[LoginGuard]},
-  {path:"help",component:HelpformComponent},
-  {path:"dashboard/newpost",component:AddpostformComponent,canActivate:[LoginGuard]},
-  {path:"login",component:LoginformComponent},
-  {path:"register",component:RegisterformComponent}
+  { path: 'posts', component: PostsComponent },
+  { path: 'posts/:id', component: SinglepostComponent },
+  { path: '', redirectTo: 'posts', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [LoginGuard] },
+  { path: 'dashboard/uploadimg', component: UploadimageComponent, canActivate: [LoginGuard] },
+  { path: 'help', component: HelpformComponent },
+  { path: 'dashboard/newpost', component: AddpostformComponent, canActivate: [LoginGuard] },
+  { path: 'login', component: LoginformComponent },
+  { path: 'register', component: RegisterformComponent }
 ];
 
 @NgModule({
